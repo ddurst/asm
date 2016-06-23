1f9bd9339487bd0edc7bd3e15631c2cdea94f355
David Durst <thedurst@gmail.com>
2016-06-22 22:18:51 -0400

    Added Horse Morsel, updated 6/16/2016

--- asm_maintenance.py
+++ asm_maintenance.py
@@ -48,13 +48,13 @@ def process_file(oldfile, newdir, start_dir, root):
                 else:
                     # operation 1: replace _metafound with block
                     if line == _metafound:
-                    	repl_template = """{line0}\t{line1}\n\t{line2}\n\t{line3}\n\t{line4}\n"""
-                    	repl_context = { "line0":_metafound,
-                    	                 "line1":'<meta property="og:site_name" content="Anti-Social Music" />', 
-                    	                 "line2":'<meta property="og:url" content="http://www.antisocialmusic.org%s" />' % _trimfile, 
-                    	                 "line3":'<meta property="og:title" content="%s" />' % _found, 
-                    	                 "line4":'<meta property="og:image" content="http://www.antisocialmusic.org/ASM_social.jpg" />'}
-                    	n.write(repl_template.format(**repl_context))
+                        repl_template = """{line0}\t{line1}\n\t{line2}\n\t{line3}\n\t{line4}\n"""
+                        repl_context = { "line0": _metafound,
+                                         "line1": '<meta property="og:site_name" content="Anti-Social Music" />', 
+                                         "line2": '<meta property="og:url" content="http://www.antisocialmusic.org%s" />' % _trimfile, 
+                                         "line3": '<meta property="og:title" content="%s" />' % _found, 
+                                         "line4": '<meta property="og:image" content="http://www.antisocialmusic.org/ASM_social.jpg" />'}
+                        n.write(repl_template.format(**repl_context))
                     else:
                         n.write(line)
     return _found
@@ -67,16 +67,16 @@ def is_candidate(file):
 
 
 try:
-	starting_directory = sys.argv[1]
+    starting_directory = sys.argv[1]
 except:
-	print 'usage: asm_maintenance.py <starting_directory>'
-	sys.exit(2)
+    print 'usage: asm_maintenance.py <starting_directory>'
+    sys.exit(2)
 
 newdir = starting_directory + '/../asm_maintenance' # a directory up so os.walk doesn't find it
 os.mkdir(newdir)
 for root, dirs, files in os.walk(starting_directory):
     for name in files:
-#		print "%s\t%s\t%s" % (root, name, os.path.join(root, name))
+#        print "%s\t%s\t%s" % (root, name, os.path.join(root, name))
         # get name, title (already have name)
         if is_candidate(name):
             full_path = os.path.join(root, name)
